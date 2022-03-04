b6cf629746bb610c4a6fb6ef4115e0436cad31ad
RivasTiziano <91221787+RivasTiziano@users.noreply.github.com>
2022-03-04 13:49:18 -0300

    SocialMediaDashBoard

---
+++ SocialMediaDB/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { SocialMediaDB } from './SocialMediaDB';
+
+function App() {
+  return (
+    <div className="App">
+      <SocialMediaDB/>
+    </div>
+  );
+}
+
+export default App;
